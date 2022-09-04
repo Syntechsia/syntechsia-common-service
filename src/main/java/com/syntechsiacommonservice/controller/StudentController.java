0b2380058ae88c1d80c80dc3497108d400844121
Jafar <mjafar.shodiq@myboost.co>
2022-09-04 20:21:18 +0700

    [FEAT] -add to pass cors

--- src/main/java/com/syntechsiacommonservice/controller/StudentController.java
+++ src/main/java/com/syntechsiacommonservice/controller/StudentController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @CrossOrigin(origins = {
-        "https://syntechsia.com/*",
-        "http://103.214.112.174:3030/*",
-        "http://103.214.112.174:8081/"
+        "https://syntechsia.com",
+        "http://103.214.112.174:3030",
+        "http://103.214.112.174:8081"
 })
 @RestController
 @RequestMapping("/student")
