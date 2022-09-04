6c6936c29cbfd99a9cacbc1b1d9e1fe4fb078c51
Jafar <mjafar.shodiq@myboost.co>
2022-09-04 20:53:18 +0700

    [FEAT] -fix to pass cors

--- src/main/java/com/syntechsiacommonservice/SyntechsiaCommonServiceApplication.java
+++ src/main/java/com/syntechsiacommonservice/SyntechsiaCommonServiceApplication.java
@@ -2,6 +2,7 @@ package com.syntechsiacommonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,7 @@ public class SyntechsiaCommonServiceApplication {
         SpringApplication.run(SyntechsiaCommonServiceApplication.class, args);
     }
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
